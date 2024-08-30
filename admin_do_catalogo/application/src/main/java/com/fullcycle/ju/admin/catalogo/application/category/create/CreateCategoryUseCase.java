41ca4599b9cf553a228934a293b7e43331075f38
Jusley Souza <jusleysouza31@gmail.com>
2024-08-29 21:53:10 -0300

    Creation and implementation of the CreateCategoryUseCase class

---
+++ admin_do_catalogo/application/src/main/java/com/fullcycle/ju/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.ju.admin.catalogo.application.category.create;
+
+import com.fullcycle.ju.admin.catalogo.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+}
