64c1625fb3d6d77c260d405a8e5782723a56dc60
Jusley Souza <jusleysouza31@gmail.com>
2024-09-03 18:08:20 -0300

    Changing the output object to an Either

--- admin_do_catalogo/application/src/main/java/com/fullcycle/ju/admin/catalogo/application/category/create/CreateCategoryUseCase.java
+++ admin_do_catalogo/application/src/main/java/com/fullcycle/ju/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -1,6 +1,8 @@
 package com.fullcycle.ju.admin.catalogo.application.category.create;
 
 import com.fullcycle.ju.admin.catalogo.application.UseCase;
+import com.fullcycle.ju.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
-public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 }
