9b2ca7773da8081b02d0e43cf8f00a623247b83b
Jusley Souza <jusleysouza31@gmail.com>
2024-09-18 09:36:41 -0300

    Creation and implementation of the test class for the category deletion use case and the delete test in case of success using TDD

---
+++ admin_do_catalogo/application/src/test/java/com/fullcycle/ju/admin/catalogo/application/category/delete/DeleteCategoryUseCaseTest.java
@@ -0,0 +1,44 @@
+package com.fullcycle.ju.admin.catalogo.application.category.delete;
+
+import com.fullcycle.ju.admin.catalogo.domain.category.Category;
+import com.fullcycle.ju.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.ju.admin.catalogo.domain.category.CategoryID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultDeleteCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp(){
+        Mockito.reset(categoryGateway);
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCategory_shouldBeOk(){
+        final var aCategory = Category.newCategory("Film", null, true);
+        final var expectedId = aCategory.getID();
+
+        doNothing().when(categoryGateway).deleteById(eq(expectedId));
+
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        Mockito.verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+    }
+
+}
