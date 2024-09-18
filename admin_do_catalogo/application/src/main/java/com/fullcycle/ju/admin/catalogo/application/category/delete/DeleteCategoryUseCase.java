d346625b8306b4d18e82e7d972c02de85c745e0d
Jusley Souza <jusleysouza31@gmail.com>
2024-09-18 09:46:03 -0300

    Creation and implementation of the Category Delete UseCase class

---
+++ admin_do_catalogo/application/src/main/java/com/fullcycle/ju/admin/catalogo/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.ju.admin.catalogo.application.category.delete;
+
+import com.fullcycle.ju.admin.catalogo.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
+}
