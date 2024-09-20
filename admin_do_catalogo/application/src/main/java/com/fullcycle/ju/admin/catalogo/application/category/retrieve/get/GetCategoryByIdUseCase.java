4c0dd1396bd677a106a1afc651ed4157d44ac7d5
Jusley Souza <jusleysouza31@gmail.com>
2024-09-20 10:25:58 -0300

    Creation and implementation the Category  getById use case class

---
+++ admin_do_catalogo/application/src/main/java/com/fullcycle/ju/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.ju.admin.catalogo.application.category.retrieve.get;
+
+import com.fullcycle.ju.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase extends UseCase<String, CategoryOutput> { }
