190917061ae292b9295fe1cfbd53df72d0973915
Jusley Souza <jusleysouza31@gmail.com>
2024-09-23 17:32:16 -0300

    Creation and implementation Web Server config class

---
+++ admin_do_catalogo/infrastructure/src/main/java/com/fullcycle/ju/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.fullcycle.ju.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcycle.ju.admin.catalogo")
+public class WebServerConfig {
+}
