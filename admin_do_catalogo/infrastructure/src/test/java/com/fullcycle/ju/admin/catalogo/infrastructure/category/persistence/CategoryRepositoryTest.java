53018eef346920303c2e568c32549fe945731da7
Jusley Souza <jusleysouza31@gmail.com>
2024-12-09 15:03:50 -0300

    Creation and implementation of the test for null createdAT using TDD

--- admin_do_catalogo/infrastructure/src/test/java/com/fullcycle/ju/admin/catalogo/infrastructure/category/persistence/CategoryRepositoryTest.java
+++ admin_do_catalogo/infrastructure/src/test/java/com/fullcycle/ju/admin/catalogo/infrastructure/category/persistence/CategoryRepositoryTest.java
@@ -33,4 +33,26 @@ public class CategoryRepositoryTest {
         Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
         Assertions.assertEquals(expectedMessage, actualCause.getMessage());
     }
+
+    @Test
+    public void givenAnInvalidNullCreatedAt_whenCallsSave_shouldReturnError() {
+        final var expectedPropertyName = "createdAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.fullcycle.ju.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity.createdAt";
+
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
+
+        final var anEntity = CategoryJpaEntity.from(aCategory);
+        anEntity.setCreatedAt(null);
+
+        final var actualException =
+                Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+
+        final var actualCause =
+                Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
+
+
 }
