fddd91e57db07ddb65df8947be33536658700305
Jusley Souza <jusleysouza31@gmail.com>
2024-12-20 15:46:04 -0300

    Creation and implementation of the create category integrated test with invalid name.

--- admin_do_catalogo/infrastructure/src/test/java/com/fullcycle/ju/admin/catalogo/application/category/create/CreateCategoryUseCaseIT.java
+++ admin_do_catalogo/infrastructure/src/test/java/com/fullcycle/ju/admin/catalogo/application/category/create/CreateCategoryUseCaseIT.java
@@ -6,8 +6,12 @@ import com.fullcycle.ju.admin.catalogo.infrastructure.category.persistence.Categ
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
 
 @IntegrationTest
 public class CreateCategoryUseCaseIT {
@@ -46,6 +50,28 @@ public class CreateCategoryUseCaseIT {
         Assertions.assertNotNull(actualCategory.getCreatedAt());
         Assertions.assertNotNull(actualCategory.getUpdatedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnDomainException() {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var aCommand =
+                CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final var notification = useCase.execute(aCommand).getLeft();
+
+        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        Mockito.verify(categoryGateway, times(0)).create(any());
     }
 
 }
